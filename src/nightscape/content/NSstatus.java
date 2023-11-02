aef2bb7511eccbdf5c61d6cc520964a0d2fafb77
SaigoN0Nozomi <118016432+SaigoN0Nozomi@users.noreply.github.com>
2023-11-03 02:41:04 +0700

    Add files via upload

--- src/nightscape/content/NSstatus.java
+++ src/nightscape/content/NSstatus.java
@@ -14,15 +14,15 @@ public class NSstatus {
 
     public static void load(){
         ozoneCorrosion = new StatusEffect("OzoneCorrosion"){{
-            speedMultiplier = 0.8f;
-            damageMultiplier = 0.8f;
+            speedMultiplier = 0.90f;
+            damageMultiplier = 0.90f;
             effect = new Effect(30, e -> {
                 randLenVectors(e.id, 1, 8f + e.fin() * 8f, (x, y) -> {
                     color(Color.valueOf("ffbdd4ff"), Color.valueOf("fc81dd22"), e.fin());
                     Fill.circle(e.x + x, e.y + y, e.fout() * 1.2f);
                 });
             });
-            damage = 0.03f;
+            damage = 0.5f / 60f;
             effectChance = 0.05f;
         }};
 
